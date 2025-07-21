4b3e15ebc96fe08b4e58ff55e11e7fd51b9e6c5b
jin mo byeon <107828109+byeonjinmo@users.noreply.github.com>
2025-07-21 14:29:13 +0900

    Update main.py

--- pf/main.py
+++ pf/main.py
@@ -4,5 +4,5 @@ import module.mymodule
 # 객체 생성
 calc = module.mymodule.FourCal()
 calc.setdata(5, 3)
-print("덧셈 결과:", calc.add())   # ✅ 인자 없이 호출
+print("덧셈 결과:", calc.add())   # 인자 없이 호출
 print("덧셈 결과:", calc.mul())
